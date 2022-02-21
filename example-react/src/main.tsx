50f843f086e6cd3cae43c66d7d995ef33fa26414
Hoang Minh Le <hoang.le@asnet.com.vn>
2022-02-21 16:57:33 +0700

    feat: make some code about composing components

--- example-react/src/main.tsx
+++ example-react/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import ClassComponents from "./example-tsx/class-components"
+import ComposingComponents from "./example-tsx/composing-components"
 import Expressions from "./example-tsx/expressions"
 import RenderingComponent from "./example-tsx/rendering-components"
 
@@ -9,6 +10,7 @@ ReactDOM.render(
     <Expressions />
     <ClassComponents />
     <RenderingComponent name="Minh Hoang" />
+    <ComposingComponents />
   </React.StrictMode>,
   document.getElementById("root")
 )

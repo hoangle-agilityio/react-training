61d1748cdfc8d8ffc0cb202f3db465585de83287
Hoang Minh Le <hoang.le@asnet.com.vn>
2022-02-21 16:57:04 +0700

    feat: make some code about class components

--- example-react/src/main.tsx
+++ example-react/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import ClassComponents from "./example-tsx/class-components"
 import Expressions from "./example-tsx/expressions"
+import RenderingComponent from "./example-tsx/rendering-components"
 
 ReactDOM.render(
   <React.StrictMode>
     <Expressions />
+    <ClassComponents />
+    <RenderingComponent name="Minh Hoang" />
   </React.StrictMode>,
   document.getElementById("root")
 )

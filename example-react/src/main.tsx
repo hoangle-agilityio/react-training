9e41080ab58d826b009332daaac3430460ca1ca9
Hoang Minh Le <hoang.le@asnet.com.vn>
2022-02-21 16:54:53 +0700

    feat: make some code about expressions

---
+++ example-react/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Expressions from "./example-tsx/expressions"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Expressions />
+  </React.StrictMode>,
+  document.getElementById("root")
+)

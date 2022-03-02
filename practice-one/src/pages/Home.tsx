97e085843f5db0fc73656f8d5d14ec4c9039a625
hoang.le <hoang.le@asnet.com.vn>
2022-03-02 08:55:21 +0700

    feat: create prototyping component

--- practice-one/src/pages/Home.tsx
+++ practice-one/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import FeatureList from "../components/FeatureList";
+import Prototyping from "../components/Prototyping";
 import TopNav from "../components/TopNav";
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <>
       <TopNav />
       <main>
         <FeatureList />
+        <Prototyping />
       </main>
     </>
   );

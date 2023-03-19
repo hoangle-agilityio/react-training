854ade285f1185018a735b152ca845f1c04abf37
hoangle-agilityio <hoang.le@asnet.com.vn>
2023-03-19 19:02:19 +0700

    feat: initial structure and config environment

---
+++ practice-react-latest-version/src/themes/typography.ts
@@ -0,0 +1,12 @@
+export const fonts = {
+  heading: "Inter SemiBold",
+  body: "Inter Regular",
+  medium: "Inter Medium",
+};
+
+export const fontSizes = {
+  lg: "22px",
+  base: "14px",
+  sm: "12px",
+  xs: "11px",
+};
